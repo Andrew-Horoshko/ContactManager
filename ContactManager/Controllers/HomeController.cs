aeec74fecaec1e3753b8621e54a642bcb028042c
unknown <safer.andy@gmail.com>
2021-02-08 09:17:23 +0200

    Added file upload functionality and some parts

--- ContactManager/Controllers/HomeController.cs
+++ ContactManager/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
 ﻿using ContactManager.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,22 +14,38 @@ namespace ContactManager.Controllers
 {
     public class HomeController : Controller
     {
+        IWebHostEnvironment _appEnvironment;
+
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController( IWebHostEnvironment appEnvironment ,ILogger<HomeController> logger)
         {
+            _appEnvironment = appEnvironment;
             _logger = logger;
         }
+
 
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Create(IFormFile uploadedFile)
         {
-            return View();
+            if (uploadedFile != null)
+            {
+                string path = "/Files/" + uploadedFile.FileName;
+                using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
+                {
+                     uploadedFile.CopyTo(fileStream);
+                }
+            }
+            ContactModel obj = new ContactModel();
+
+            obj.OpenFile(uploadedFile);
+            return View(obj);
         }
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ ContactManager/Models/ContactModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactManager.Models
+{
+    public class ContactModel
+    {
+        public  string file { get; set; }
+
+        public void OpenFile(IFormFile uploadedFile)
+        {
+            string path = @"wwwroot\Files\" + uploadedFile.FileName ;
+             file = File.ReadAllText(path);
+        }
+    }
+}
