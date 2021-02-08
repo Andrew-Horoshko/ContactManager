e801e53d3fac1f73556208e484ee54ed40e31bdb
unknown <safer.andy@gmail.com>
2021-02-08 15:14:31 +0200

    Connected to db , made a table for db values

--- ContactManager/Controllers/HomeController.cs
+++ ContactManager/Controllers/HomeController.cs
@@ -9,19 +9,24 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using ContactManager.Classes;
 
 namespace ContactManager.Controllers
 {
     public class HomeController : Controller
     {
         IWebHostEnvironment _appEnvironment;
-
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        SqlConnection con = new SqlConnection();
         private readonly ILogger<HomeController> _logger;
-
+        List<Person> contactsList = new List<Person>();
         public HomeController( IWebHostEnvironment appEnvironment ,ILogger<HomeController> logger)
         {
             _appEnvironment = appEnvironment;
             _logger = logger;
+            con.ConnectionString = ContactManager.Properties.Resources.ConnectionString;
         }
 
 
@@ -43,9 +48,38 @@ namespace ContactManager.Controllers
             ContactModel obj = new ContactModel();
 
             obj.OpenFile(uploadedFile);
-            return View(obj);
+            FetchData();
+            return View(contactsList);
         }
-        
+        private void FetchData()
+        {
+            if (contactsList.Count > 0) contactsList.Clear();
+            try
+            {
+                con.Open();
+                com.Connection = con;
+                com.CommandText = "SELECT TOP(100)[Id] ,  [Name],[Date of birth], [Married], [Phone] ,[Salary] FROM [Apps].[dbo].[Contact]";
+                dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    contactsList.Add(new Person()
+                    {
+                        Id = dr["Id"].ToString(),
+                        Name = dr["Name"].ToString(),
+                        DateOfBirth = dr["Date of birth"].ToString(),
+                        Married = dr["Married"].ToString(),
+                        Phone = dr["Phone"].ToString(),
+                        Salary = dr["Salary"].ToString()
+                    });
+                }
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
